c1daabc1af798bde6ef4840f4302419ace1a43be
lixi <lixi@itcast.cn>
2021-04-23 19:35:59 +0800

    初始化

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,4 +2,10 @@
 $.ajaxPrefilter(function(options) {
     //统一设置请求的url地址，根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+        //url中包含my说明有权限接口
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function() {
+    //提取用户基本信息
+    getUserInfo()
+
+    function getUserInfo() {
+        $.ajax({
+            method: 'get',
+            url: '/my/userinfo',
+
+            success: function(res) {
+                // console.log(res);
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取用户信息失败')
+                }
+
+                renderAvatar(res.data)
+
+
+
+            }
+        })
+    }
+
+    function renderAvatar(user) {
+        //获取用户的名称（有nickname就用nickname，没有就用username）
+        var name = user.nickname || user.username
+            //渲染欢迎用语
+
+        $('#welcom').html('欢迎' + name)
+            //渲染头像（有头像图像则渲染，没有则渲染文字）
+
+        if (user.user_pic !== null) {
+            $('.layui-nav-img').attr('src', user.user_pic).show()
+            $('.text-avatar').hide()
+        } else {
+            var first = name[0].toUpperCase()
+            $('.text-avatar').html(first).show()
+            $('.layui-nav-img').hide()
+
+        }
+    }
+})
