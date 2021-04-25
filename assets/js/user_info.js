3636883a218d1e98a5aae87e8bd0a51314d2aed3
lixi <lixi@itcast.cn>
2021-04-25 14:25:24 +0800

    基本资料

--- assets/js/user_info.js
+++ assets/js/user_info.js
@@ -16,7 +16,62 @@ $(function() {
                     return layui.layer.msg(res.message, { icon: 5 })
 
                 }
+                //把用户信息展示到表单中
+                // $('[name=username').val(res.data.username)
+                // $('[name=nickname').val(res.data.nickname)
+                // $('[name=email').val(res.data.email)
+                // $('[name=id').val(res.data.id)
+
+                //快速给所有的表单项赋值
+                layui.form.val('formUserInfo', res.data)
             }
         })
     }
+
+
+    //2-重置表单信息
+    //效果：重新把用户展示到表单中
+    $('#btnReset').on('click', function(e) {
+        //阻止默认行为（重置按钮的默认行为是清空表单项）
+        e.preventDefault()
+            //重新获取用户信息，重新赋值
+        initUserInfo()
+    })
+
+    //自定义一个验证昵称长度规则
+    layui.form.verify({
+        nickname: function(value, item) {
+            if (value.length > 6) {
+                return '昵称不能超过六个字'
+            }
+
+        }
+
+
+    })
+
+
+    //完成用户修改功能
+    $('.layui-btn').on('submit', function(e) {
+
+        e.preventDefault()
+            //获取输入框中的内容
+        var data = $(this).serialize()
+        console.log(data);
+        $.ajax({
+            method: 'post',
+            url: '/my/userinfol',
+            data: data,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg(res.message, { icon: 5 })
+                }
+                layui.layer.msg(res.message, { icon: 6 })
+
+
+            }
+        })
+    })
+
+
 })
